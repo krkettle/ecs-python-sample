34d6e5cba02dccac093193da7dd182ddd198bde1
krkettle <catrarazou@gmail.com>
2021-03-13 13:54:49 +0900

    [add] s3 api

--- api/src/main.py
+++ api/src/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from typing import Dict
+from typing import Any, Dict
+import json
+import boto3
 
 app = FastAPI()
 
@@ -7,3 +9,13 @@ app = FastAPI()
 @app.get("/")
 async def root() -> Dict[str, str]:
     return {"message": "Hello World"}
+
+
+@app.get("/s3")
+async def s3_timestamp() -> Any:
+    clinet = boto3.client("s3")
+    BUCKET_NAME = "docker-python-sample-bucket"
+    OBJECT_KEY = "sample.json"
+    res = clinet.get_object(Bucket=BUCKET_NAME, Key=OBJECT_KEY)
+    body = res["Body"].read()
+    return json.loads(body.decode("utf-8"))
